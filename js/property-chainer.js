a0d99dc54fbce69291d79c754dada73e738ccd72
Steven Olmsted <steven.olmsted@ibigroup.com>
2016-12-30 07:00:47 -0500

    Combine similar methods

--- js/property-chainer.js
+++ js/property-chainer.js
@@ -1,14 +1,15 @@
+/* eslint-disable indent */
+
 import _make from 'isotropic-make';
 import _prototypeChain from 'isotropic-prototype-chain';
 
-const _PropertyChainer = _make({
-    _init ({
+const _getInitMethod = breakValue => function ({
         propertyChainsPropertyName = '_propertyChains'
     } = {}) {
         const completedPropertyChains = new Set();
 
         for (const object of _prototypeChain(this)) {
-            if (object === Object.prototype) {
+            if (object === breakValue) {
                 break;
             }
 
@@ -29,12 +30,12 @@ const _PropertyChainer = _make({
 
         return this;
     },
-    _initPropertyChain (propertyName) {
+    _getInitPropertyChainMethod = breakValue => function (propertyName) {
         let object,
             previousProperty;
 
         for (object of _prototypeChain(this)) {
-            if (object === Object.prototype) {
+            if (object === breakValue) {
                 break;
             }
 
@@ -56,63 +57,14 @@ const _PropertyChainer = _make({
         }
 
         return this;
-    }
-}, {
-    _init ({
-        propertyChainsPropertyName = '_propertyChains'
-    } = {}) {
-        const completedPropertyChains = new Set();
-
-        for (const object of _prototypeChain(this)) {
-            if (object === Object) {
-                break;
-            }
-
-            if (Reflect.apply(Object.prototype.hasOwnProperty, object, [
-                propertyChainsPropertyName
-            ])) {
-                for (const propertyName of object[propertyChainsPropertyName]) {
-                    if (completedPropertyChains.has(propertyName)) {
-                        // TODO: log dev-time warning
-                        continue;
-                    }
-
-                    this._initPropertyChain(propertyName);
-                    completedPropertyChains.add(propertyName);
-                }
-            }
-        }
-
-        return this;
     },
-    _initPropertyChain (propertyName) {
-        let object,
-            previousProperty;
 
-        for (object of _prototypeChain(this)) {
-            if (object === Object) {
-                break;
-            }
-
-            if (Reflect.apply(Object.prototype.hasOwnProperty, object, [
-                propertyName
-            ])) {
-                const property = object[propertyName];
-
-                if (previousProperty) {
-                    if (Reflect.getPrototypeOf(previousProperty) === property) {
-                        break;
-                    }
-
-                    Reflect.setPrototypeOf(previousProperty, property);
-                }
-
-                previousProperty = property;
-            }
-        }
-
-        return this;
-    }
-});
+    _PropertyChainer = _make({
+        _init: _getInitMethod(Object.prototype),
+        _initPropertyChain: _getInitPropertyChainMethod(Object.prototype)
+    }, {
+        _init: _getInitMethod(Object),
+        _initPropertyChain: _getInitPropertyChainMethod(Object)
+    });
 
 export default _PropertyChainer;
